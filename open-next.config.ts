234dfeea6d937240a97062779924406affff74c9
ntpten-x <natthaphongcontact@gmail.com>
2026-07-15 12:13:42 +0700

    add pg-cloudflare and expo-sqlite to opennext buildConfig external

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,3 +1,9 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
-export default defineCloudflareConfig();
+export default defineCloudflareConfig({
+  dangerous: {
+    buildConfig: {
+      external: ["pg-cloudflare", "expo-sqlite"],
+    },
+  },
+});
